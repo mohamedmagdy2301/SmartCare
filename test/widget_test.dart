f1953099e0df80a8ef6a32bbf972c244f888b301
Mohamed magdy <mohammedmego15@gmail.com>
2024-11-22 16:04:26 +0200

    Fix Bugs run development flavors android in app ✅

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:smartcare/smartcare_app/smartcare_app.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const SmartCareApp());
+    await tester.pumpWidget(const SmartCareApp(isProduction: false));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
